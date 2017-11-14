4d7cf1ff5e0a11f786a1bc91d994c5fa909b28b1
东往 <wgzhxy@163.com>
2017-11-14 19:39:15 +0800

    spring-boot,  增加debug模式

--- app/src/main/java/org/wanggz/begin/app/HelloController.java
+++ app/src/main/java/org/wanggz/begin/app/HelloController.java
@@ -1,6 +1,9 @@
 package org.wanggz.begin.app;
 
 import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,24 @@ import java.util.stream.Stream;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hellohello")
+    @Value("${book.name}")
+    String name;
+
+    @Value("${book.author}")
+    String author;
+
+    @Value("${book.desc}")
+    String desc;
+
+    private Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping("/hello")
     public String index() {
+
+        LOG.info("name : " + name);
+        LOG.info("author : " + author);
+        LOG.info("desc : " + desc);
+
         List<String> ls = Stream.of("test", "test01", "test02", "test03", "test04").collect(Collectors.toList());
         return JSON.toJSONString(ls);
     }
