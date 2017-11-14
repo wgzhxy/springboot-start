5de4f0e5d7b96be4bc11ee2c6229f4e8027d874b
东往 <wgzhxy@163.com>
2017-11-14 11:14:35 +0800

    初始化begin项目

---
+++ begin/src/main/java/org/wanggz/springboot/begin/BeginApplication.java
@@ -0,0 +1,12 @@
+package org.wanggz.springboot.begin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeginApplication.class, args);
+    }
+}
