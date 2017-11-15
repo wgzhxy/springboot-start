5a88895fabf5c50738da1ac28b2783c4b204740e
东往 <wgzhxy@163.com>
2017-11-15 10:21:14 +0800

    增加 client模块, service模块

---
+++ service/src/main/java/org/wanggz/springboot/HelloApplication.java
@@ -0,0 +1,13 @@
+package org.wanggz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ service/src/main/java/org/wanggz/springboot/HelloController.java
@@ -0,0 +1,27 @@
+package org.wanggz.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello, host : " + instance.getHost() + ", service_id : " + instance.getServiceId());
+        return "Hello World";
+    }
+
+}
