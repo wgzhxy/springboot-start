1ffd7670b6307a542db2fb9e623fdbafe75299d0
东往 <wgzhxy@163.com>
2017-11-16 15:53:10 +0800

    服务消费

--- consumer/src/main/java/org/wanggz/springboot/consumer/ConsumerApplication.java
+++ consumer/src/main/java/org/wanggz/springboot/consumer/ConsumerApplication.java
@@ -1,6 +1,5 @@
 package org.wanggz.springboot.consumer;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
