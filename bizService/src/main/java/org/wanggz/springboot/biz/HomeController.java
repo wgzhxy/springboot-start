1f0ea8ad6fd38d1bec178315af16ee6c3280131c
东往 <wgzhxy@163.com>
2017-11-17 16:44:40 +0800

    配置bugfix, 增加测试及产品环境

--- bizService/src/main/java/org/wanggz/springboot/biz/HomeController.java
+++ bizService/src/main/java/org/wanggz/springboot/biz/HomeController.java
@@ -1,13 +1,18 @@
 package org.wanggz.springboot.biz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private static Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/")
     public String index() {
+        LOG.info("index ---------------------------");
         return "index";
     }
 }
