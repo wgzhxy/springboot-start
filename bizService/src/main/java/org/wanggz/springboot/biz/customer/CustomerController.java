fea5e108491345ef731d63933d010e2c5253e548
东往 <wgzhxy@163.com>
2017-11-17 09:55:48 +0800

    更新path

--- bizService/src/main/java/org/wanggz/springboot/biz/customer/CustomerController.java
+++ bizService/src/main/java/org/wanggz/springboot/biz/customer/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
     @RequestMapping("/customer")
