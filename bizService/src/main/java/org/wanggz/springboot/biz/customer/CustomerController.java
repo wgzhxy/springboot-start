cce218aa16c570bd5bc38e0afd50277c65483840
东往 <wgzhxy@163.com>
2017-11-17 21:42:45 +0800

    新增方法

--- bizService/src/main/java/org/wanggz/springboot/biz/customer/CustomerController.java
+++ bizService/src/main/java/org/wanggz/springboot/biz/customer/CustomerController.java
@@ -1,15 +1,45 @@
 package org.wanggz.springboot.biz.customer;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/rest")
 public class CustomerController {
 
-    @RequestMapping("/customer")
+    @RequestMapping(value = "/customer", produces = "text/plain;charset=UTF-8")
     public String home() {
         return "Hello World，Good One!";
+    }
+
+    @RequestMapping(value = "/getCustomerJson", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public Customer getCustomerForjson() {
+        Customer customer = new Customer();
+        customer.setAddress("文一西路965号");
+        customer.setAge(25);
+        customer.setName("光忠");
+        customer.setNickName("东往");
+        return customer;
+    }
+
+    @RequestMapping(value = "/getCustomerXml", produces = "application/xml;charset=UTF-8")
+    @ResponseBody
+    public Customer getCustomerForXml() {
+        Customer customer = new Customer();
+        customer.setAddress("文一西路969号");
+        customer.setAge(30);
+        customer.setName("guangzhong");
+        customer.setNickName("john");
+        return customer;
+    }
+
+    @RequestMapping(value = "/{params_1}/{params_2}/getCustomerXml", produces = "application/xml;charset=UTF-8")
+    @ResponseBody
+    public String getString(@PathVariable String params_1, @PathVariable String params_2) {
+        return "url -> params_1 : " + params_1 + "--- : params_2 : " + params_2;
     }
 }
